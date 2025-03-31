d2a7888d17053eece047e321b697196c54cf93ba
Anish <anishdharnidhar07@gmail.com>
2025-03-31 14:40:28 +0530

    env changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 //middleware to check auth using key
-
+require("dotenv").config()
 const authCheck = (req,res,next) =>{
     const authKey = req.headers['auth'];
 
-    if(authKey !== "ZjVGZPUtYW1hX2FuZHJvaWRfMjAyMzY0MjU="){
+    if(authKey !== process.env.AUTH){
         return res.status(403).send({message : "Authentication Failed"})
     }
     next()
